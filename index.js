31c853dba222923343abbb89e37ad9abfba2a26e
Ross Keeley <rosskeeley@jennifers-mbp.home>
2020-12-17 21:41:39 +0000

    Added prevous and next functions to carousel

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// javascript
-
 var cards = document.getElementsByClassName("card");
-var slide = 0;
-var moving = false;
-
-// function setInitialClasses () {
-//     cards[cards.length - 1].classList.add("prevCard");
-//     cards[0].classList.add("active");
-//     cards[1].classList.add("nextCard");
-// }
+var cardsArray = [...cards];
 
 function setEventListeners () {
     var next = document.getElementsByClassName("next")[0];
@@ -19,57 +10,14 @@ function setEventListeners () {
 }
 
 function moveNext() {
-    if(!moving) {
-        disableInteraction();
-        cards.unshift(cards[cards.length - 1]); 
-        cards.pop();
-    //     if (slide === (cards.length - 1)) {
-    //         slide = 0;
-    //     } else {
-    //         slide++;
-    //     }
-    }
+        cardsArray.unshift(cardsArray[cardsArray.length - 1]);
+        cardsArray.pop();
+        document.getElementsByClassName("gallery")[0].innerHTML = `<img src="${cardsArray[0].src}">`;
 }
 
 function movePrev() {
-    if(!moving) {
-        disableInteraction();
-        cards.push(cards[0]);
-        cards.shift();
-        // if (slide === 0) {
-        //     slide = (cards.length - 1);
-        // } else {
-        //     slide--;
-        // }
-    }
+        cardsArray.push(cardsArray[0]);
+        cardsArray.shift();
+        document.getElementsByClassName("gallery")[0].innerHTML = `<img src="${cardsArray[0].src}">`;
 }
-
-function disableInteraction() {
-    moving = true;
-    setTimeout(function() {
-        moving = false;
-    }, 500);
-}
-
-function moveForward() {
-    disableInteraction();
-    cards.unshift(cards[cards.length - 1]); 
-    cards.pop();   
-}
-
-function moveBack() {
-    disableInteraction();
-    cards.push(cards[0]);
-    cards.shift();
-}
-
-// function moveCarouselTo(slide) {
-//     if(!moving) {
-//         disableInteraction();
-        
-//         var newPrev = slide - 1;
-//         var newNext = slide + 1;
-//         var oldPrev = slide - 2;
-//     }
-// }
-
+setEventListeners();
